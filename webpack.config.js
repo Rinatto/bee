75e60fd52e74e16bea4a22400682fd4b8dbb4874
Apple <apple@MacBook-Pro-Apple.local>
2024-05-19 22:13:13 +0300

    Move build files to root

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    publicPath: '/bee/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/images/favicon.png',
+    }),
+    new Dotenv(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/activity.html', to: 'activity.html' },
+        { from: 'src/map.html', to: 'map.html' },
+        { from: 'src/timer.html', to: 'timer.html' },
+        { from: 'src/resume.html', to: 'resume.html' },
+        { from: 'src/images', to: 'images' }, 
+      ],
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
+  mode: 'development'
+};
