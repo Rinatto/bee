cd81489f847e695fe8ba82dc2bfc6437b1b7ed31
Apple <apple@MacBook-Pro-Apple.local>
2024-05-21 23:32:08 +0300

    Скопированы файлы из коммита 795d6402adf9242eea25d45f7eba436a7f3fe121 ветки development

---
+++ src/scripts/yandexMap.js
@@ -0,0 +1,51 @@
+let map;
+let mapInitialized = false;
+
+export function initializeYandexMap() {
+  const mapContainer = document.getElementById("yandexMap");
+  if (mapContainer && !mapInitialized) {
+    const apiKey = process.env.APP_MAPS_API_KEY;
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = `https://api-maps.yandex.ru/2.1/?apikey=${apiKey}&lang=ru_RU`;
+    script.onload = function () {
+      ymaps.ready(init);
+      mapInitialized = true; // Устанавливаем флаг, что карта загружена
+      hideSpinner();
+    };
+    mapContainer.appendChild(script);
+  } else if (mapInitialized) {
+    restoreMap();
+    hideSpinner();
+  } else {
+    console.error("Map container not found!");
+  }
+}
+
+function init() {
+  map = new ymaps.Map("yandexMap", {
+    center: [59.843996, 30.360796], // Задание центра карты
+    zoom: 14,
+  });
+
+  const myPlacemark = new ymaps.Placemark([59.843996, 30.360796], {
+    balloonContent: 'Домашний адрес'
+  });
+
+  map.geoObjects.add(myPlacemark);
+}
+
+function restoreMap() {
+  const mapContainer = document.getElementById("yandexMap");
+  if (mapContainer) {
+    mapContainer.appendChild(map.container.getElement());
+  }
+}
+
+function hideSpinner() {
+  const spinner = document.querySelector(".rotate-svg");
+  if (spinner) {
+    spinner.style.display = "none"; // Скрываем спиннер
+  }
+}
