02d7f1876aaacee6797ffe1171d3bf7d1f091ee2
Apple <apple@MacBook-Pro-Apple.local>
2024-05-22 00:10:01 +0300

    add root file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'root'),
     filename: 'bundle.[contenthash].js',
     publicPath: process.env.PUBLIC_PATH || '/'
   },
